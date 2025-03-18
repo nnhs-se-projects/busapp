7ae6c06b496619eabacb386462f5e6c59d93e1c8
LucasVL <lucas@lucasvl.nl>
2025-03-18 14:16:04 -0500

    fix announcements again while im at it

--- static/ts/updateAnnouncementConnect.js
+++ static/ts/updateAnnouncementConnect.js
@@ -7,9 +7,7 @@ adminSocket.on("update", (data) => {
 
     console.log(data)
 
-    window.location.reload();
-    
-
+    //window.location.reload();
 });
 
 function updateAnnouncement() {
@@ -17,4 +15,35 @@ function updateAnnouncement() {
     adminSocket.emit("updateMain", {
         type: "update",
     });
+}
+
+function clearAnnouncement() {
+    document.getElementsByName("announcement")[0].value = "";
+    save_();
+}
+
+function clearTvAnnouncement() {
+    document.getElementsByName("tvAnnouncement")[0].value = "";
+    save_();
+}
+announcement = document.getElementsByName("announcement")[0].value;
+tvAnnouncement = document.getElementsByName("tvAnnouncement")[0].value;
+
+async function save_() {
+    await fetch("/submitAnnouncement", {
+        method: 'POST',
+        headers: {
+            accept: 'application.json',
+            'Content-Type': 'application/json'
+        },
+        body:
+        JSON.stringify({
+            announcement: document.getElementsByName("announcement")[0].value,
+            tvAnnouncement: document.getElementsByName("tvAnnouncement")[0].value
+        })
+    });
+
+    updateAnnouncement();
+
+    window.location.href = "/admin";
 }
