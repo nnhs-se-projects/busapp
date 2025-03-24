95b0a3c30d5efb4931124cf16ce241945a3020b3
Aidan Malana <aidanmalana02@gmail.com>
2025-03-24 09:31:44 -0500

    Refactor weather data handling and update UI to display new forecast information including image
    Background doesn't blur yet

--- server/weatherController.js
+++ server/weatherController.js
@@ -5,13 +5,15 @@ const fetch = require ("node-fetch");
 // Code to update weather automcatically every 5 minutes
 async function getWeather(io) {
     try {
-        const res = await fetch("http://api.weatherapi.com/v1/current.json?"
-            + new URLSearchParams([["key", "8afcf03c285047a1b6e201401222202"], ["q", "60563"]]
-            ));
+        // If for some reason Naperville North's coordinates change a significant amount, becoming 
+        // Aurora North, or god forbid, we merge with Naperville Central after the apocalypse, 
+        // and Naperville Central/North then moves to another city, you might need to change the gridpoint. 
+        // Currently we are getting this url from https://api.weather.gov/points/41.7835,-88.1573
+        const res = await fetch("https://api.weather.gov/gridpoints/LOT/58,68/forecast/hourly");
         await writeWeather(await res.json());
         io.of("/admin").emit("updateWeather", (await readData()).weather);
     } catch (error) {
-        console.log('failed to fetch data from weatherapi.com', error);
+        console.log('failed to fetch data from weather.gov', error);
     }
 
 
