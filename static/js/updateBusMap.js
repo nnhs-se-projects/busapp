31f42c8b7fed63d845c6fafb3605e9d5fc158ba2
myulbarsov <myulbarsov@stu.naperville203.org>
2025-04-23 10:29:18 -0500

    Continue implementing the color change and lots

--- static/js/updateBusMap.js
+++ static/js/updateBusMap.js
@@ -7,14 +7,6 @@ const nextContent = document.getElementById("next-wave").innerHTML;
 const currentContent = document.getElementById("current-wave").innerHTML;
 const lotsContent = document.getElementById("bus-lots").innerHTML;
 
-
-/*
-These are to get the actual list of buses in each wave
-*/
-const nextWave = Array.from(document.getElementById("next-wave").rows).map(row => Array.from(row.cells).map(cell => cell.innerText));
-const currentWave = document.getElementById("current-wave");
-
-
 const pins = localStorage.getItem("pins");
 
 const button = document.getElementById("button map-change");
@@ -29,39 +21,25 @@ document.getElementById("bus-lots").innerHTML = "";
 function changeMap() {
    if (document.getElementById("next-wave").innerHTML !== "") {
 
-
        document.getElementById("next-wave").innerHTML = "";
        document.getElementById("current-wave").innerHTML = "";
-
 
        document.getElementById("bus-lots").innerHTML = lotsContent;
 
        button.innerText = "Waves";
-       
    }
    else {
        document.getElementById("next-wave").innerHTML = nextContent;
        document.getElementById("current-wave").innerHTML = currentContent;
 
-
        document.getElementById("bus-lots").innerHTML = "";
-
 
        button.innerText = "Lots";
    }
 }
 
 
-function containsPins() {
-   for (let i = 0; i < currentWave.length; i++) {
-       if (pins.includes(nextWave[i].value)) {
-           nextWave[i].value = "pinned";
-       }
-   }
-}
-
-
-containsPins();
+const getPins = () => { return pins };
 
 
 async function saveMap() {
