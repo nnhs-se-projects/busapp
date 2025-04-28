70bd9b7d15c4a8b4a41ea4aa0b1232cd872cd849
myulbarsov <myulbarsov@stu.naperville203.org>
2025-04-28 09:34:31 -0500

    Look at updateBusList for reference on how to do the lots.

--- static/js/updateBusMapAdmin.js
+++ static/js/updateBusMapAdmin.js
@@ -1,34 +1,19 @@
 "use strict";
-window.onload = () => { updateLots(); }
+window.onload = () => { document.getElementById("addInput").value = ""; }
 
 const rowA = document.getElementsByClassName("rowA");
 const rowB = document.getElementsByClassName("rowB");
 
 const test = [1,2,3,4,5,6,7,8,9,10,11,12,13,14];
 
-function updateLots() {
-    for (let i = 0; i < rowA.length; i++) {
-        rowA[i].textContent = test[i];
-    }
-    for (let i = 0; i < rowB.length; i++) {
-        rowB[i].textContent = test[i + 14];
-    }
-}
+async function save(elem) {
 
-async function saveMap() {
-    await fetch("/updateBusMap", {
+    const bus = elem.parentElement.parentElement.querySelector("input").value;
+    await fetch("/updateBusMapAdmin", {
         method: 'POST',
-        headers: {
-            accept: 'application.json',
-            'Content-Type': 'application/json'
-        },
-        body:
-        JSON.stringify({
-            lots, lots
-        })
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ bus: bus })
     });
- 
- 
-    updateBusMap();
-    window.location.assign("/admin");
+
+    updateBusList();
 }
